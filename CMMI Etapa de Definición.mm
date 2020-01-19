457d278c707d6dfd7f1b60df9fd37a1a826a36d0
Pablo Sao <pablo_sao@outlook.com>
2020-01-19 17:41:39 -0600

    Update CMMI Etapa de Definición.mm

--- CMMI Etapa de Definición.mm
+++ CMMI Etapa de Definición.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.7.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="CMMI&#xa;Etapa de Definici&#xf3;n" FOLDED="false" ID="ID_1068061441" CREATED="1579449170803" MODIFIED="1579449370580" STYLE="oval">
+<node TEXT="CMMI&#xa;Etapa de Definici&#xf3;n" FOLDED="false" ID="ID_1068061441" CREATED="1579449170803" MODIFIED="1579476144737" STYLE="oval">
 <font SIZE="18"/>
 <hook NAME="MapStyle">
     <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_note_icons="true"/>
@@ -74,7 +74,7 @@
 <node TEXT="Requerimientos" POSITION="left" ID="ID_1541562144" CREATED="1579449574554" MODIFIED="1579462053428">
 <edge COLOR="#00ff00"/>
 <node TEXT="Requerimientos del cliente" ID="ID_863495538" CREATED="1579458730227" MODIFIED="1579465334767">
-<node TEXT="Identificar las Necesidades" FOLDED="true" ID="ID_1352571100" CREATED="1579459559968" MODIFIED="1579470329522"><richcontent TYPE="DETAILS">
+<node TEXT="Identificar las Necesidades" ID="ID_1352571100" CREATED="1579459559968" MODIFIED="1579470329522"><richcontent TYPE="DETAILS">
 
 <html>
   <head>
@@ -218,8 +218,8 @@
 <node TEXT="Funci&#xf3;n de los Requisitos" ID="ID_546320865" CREATED="1579469714420" MODIFIED="1579469753955"/>
 <node TEXT="Facilitar la Producci&#xf3;n" ID="ID_572993698" CREATED="1579469760090" MODIFIED="1579469775815"/>
 </node>
-<node TEXT="Identificar los Requerimientos de Interfaz" ID="ID_898190017" CREATED="1579468629064" MODIFIED="1579470417013">
-<arrowlink SHAPE="CUBIC_CURVE" COLOR="#cc3300" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1899353590" STARTINCLINATION="355;0;" ENDINCLINATION="510;82;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="Identificar los Requerimientos de Interfaz" ID="ID_898190017" CREATED="1579468629064" MODIFIED="1579477167850">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#cc3300" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1899353590" SOURCE_LABEL="Soluciones que se plantean" STARTINCLINATION="429;64;" ENDINCLINATION="655;161;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
 <richcontent TYPE="DETAILS">
 
 <html>
@@ -241,21 +241,266 @@
 </richcontent>
 </node>
 </node>
-<node TEXT="Analizar y validar los Requerimientos" ID="ID_1000504692" CREATED="1579458759698" MODIFIED="1579470057510"/>
+<node TEXT="Analizar y Valodar los Requerimientos" ID="ID_1442842051" CREATED="1579475589502" MODIFIED="1579475598652">
+<node TEXT="Establecer Conceptos y Esenarios" ID="ID_1000504692" CREATED="1579458759698" MODIFIED="1579476500999"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#003399">Eventos que pueden suceder en el </font>
+    </p>
+    <p>
+      <font color="#003399">desarrollo, uso o soporte del producto.</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Forma de Operaci&#xf3;n" ID="ID_273922185" CREATED="1579476207910" MODIFIED="1579476223160"/>
+<node TEXT="Desarrollo" ID="ID_993577455" CREATED="1579476239928" MODIFIED="1579476264807"/>
+<node TEXT="Instalaci&#xf3;n" ID="ID_1470369390" CREATED="1579476268030" MODIFIED="1579476271821"/>
+<node TEXT="Operaci&#xf3;n" ID="ID_643848154" CREATED="1579476274920" MODIFIED="1579476278440"/>
+<node TEXT="Mantenimiento y Soporte" ID="ID_1100723131" CREATED="1579476280935" MODIFIED="1579476285860"/>
+<node TEXT="Capacitaci&#xf3;n y Retirada" ID="ID_90680818" CREATED="1579476289405" MODIFIED="1579476294630"/>
+<node TEXT="Casos de Uso" ID="ID_84253464" CREATED="1579476342180" MODIFIED="1579476811992">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#990099" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_316781762" SOURCE_LABEL="Se incluye" STARTINCLINATION="52;20;" ENDINCLINATION="183;150;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#003399">Acciones de una empresa que da </font>
+    </p>
+    <p>
+      <font color="#003399">un resultado de valor observable.</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Funcionalidad y Atributos" ID="ID_1155496873" CREATED="1579475643917" MODIFIED="1579476928333"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#003399">Como debe de funcionar el producto </font>
+    </p>
+    <p>
+      <font color="#003399">en base a las necesidades del </font>
+    </p>
+    <p>
+      <font color="#003399">negocio. Como las acciones, secuencia, </font>
+    </p>
+    <p>
+      <font color="#003399">entradas y salidas.</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Arquitectura" ID="ID_1792983273" CREATED="1579476621106" MODIFIED="1579476639235"/>
+<node TEXT="Diagramas de Actividad" ID="ID_316781762" CREATED="1579476645368" MODIFIED="1579476666815"/>
+</node>
+<node TEXT="Analizar Requisitos" ID="ID_1745819096" CREATED="1579475666250" MODIFIED="1579476974051"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#003399">Determinar si son necesarios y suficientes </font>
+    </p>
+    <p>
+      <font color="#003399">para cumplir con los objetivos del producto.</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="" ID="ID_1970478122" CREATED="1579477065041" MODIFIED="1579477065041">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Validar los Requisitos" ID="ID_1231150688" CREATED="1579475673999" MODIFIED="1579477062425"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#003399">Evaluar los riesgos de los requisitos </font>
+    </p>
+    <p>
+      <font color="#003399">en base a las necesidades.</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="" ID="ID_843683858" CREATED="1579477065041" MODIFIED="1579477065041">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node ID="ID_1251230331" CREATED="1579477065041" MODIFIED="1579477215090"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666"><b>Costo</b></font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_332048494" CREATED="1579477071830" MODIFIED="1579477225673"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#006666">Calendario de Entregas</font></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1405568505" CREATED="1579477078864" MODIFIED="1579477240530"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#006666">Prioridades</font></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_565623391" CREATED="1579477092104" MODIFIED="1579477253131"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666"><b>Reutilizaci&#243;n de Componentes</b></font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_961949062" CREATED="1579477102522" MODIFIED="1579477264475"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#006666">Facilidad de Mantenimiento y Soporte</font></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
 </node>
 <node TEXT="" POSITION="left" ID="ID_902655162" CREATED="1579464792901" MODIFIED="1579464851091">
 <edge COLOR="#0000ff"/>
 <hook NAME="SummaryNode"/>
 <hook NAME="AlwaysUnfoldedNode"/>
-<node TEXT="Necesidades" ID="ID_441499802" CREATED="1579464792901" MODIFIED="1579464825433"/>
-<node TEXT="Expectativas" ID="ID_1386130093" CREATED="1579464795158" MODIFIED="1579464829944"/>
-<node TEXT="Restricciones" ID="ID_434333755" CREATED="1579464830382" MODIFIED="1579464855414"/>
-<node TEXT="Entregas" ID="ID_932959688" CREATED="1579469846482" MODIFIED="1579469849846"/>
+<node ID="ID_441499802" CREATED="1579464792901" MODIFIED="1579477273514"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666"><b>Necesidades</b></font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1386130093" CREATED="1579464795158" MODIFIED="1579477280413"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666"><b>Expectativas</b></font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_434333755" CREATED="1579464830382" MODIFIED="1579477286618"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666"><b>Restricciones</b></font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_932959688" CREATED="1579469846482" MODIFIED="1579477292735"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666"><b>Entregas</b></font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
 </node>
 <node TEXT="Gesti&#xf3;n de Riesgos" POSITION="right" ID="ID_1655530842" CREATED="1579449593556" MODIFIED="1579449755169">
 <edge COLOR="#ff00ff"/>
 </node>
-<node TEXT="Soluci&#xf3;n T&#xe9;cnica" POSITION="left" ID="ID_1899353590" CREATED="1579449769104" MODIFIED="1579470005134">
+<node TEXT="Soluci&#xf3;n T&#xe9;cnica" POSITION="left" ID="ID_1899353590" CREATED="1579449769104" MODIFIED="1579476144736">
 <edge COLOR="#00ffff"/>
 </node>
 <node TEXT="Validaci&#xf3;n" POSITION="right" ID="ID_1297732119" CREATED="1579449789182" MODIFIED="1579449796789">
